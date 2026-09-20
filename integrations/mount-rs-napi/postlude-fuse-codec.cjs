0981bccf55bdef74c281fe37a6d9e692340709da
Andrew McClenaghan <andymac4182@users.noreply.github.com>
2026-09-21 09:18:35 +1000

    feat(napi): publish FUSE IOCTL codec packet (postlude-fuse-codec.cjs)

--- integrations/mount-rs-napi/postlude-fuse-codec.cjs
+++ integrations/mount-rs-napi/postlude-fuse-codec.cjs
@@ -720,12 +720,10 @@ function install(binding) {
     ["decodeStatfsIn", "fuseDecodeStatfsIn"], ["encodeStatfsIn", "fuseEncodeStatfsIn"],
     ["decodeStatfsOut", "fuseDecodeStatfsOut"], ["encodeStatfsOut", "fuseEncodeStatfsOut"],
     ["decodeInterruptIn", "fuseDecodeInterruptIn"], ["encodeInterruptIn", "fuseEncodeInterruptIn"],
+    ["decodeIoctlIn", "fuseDecodeIoctlIn"], ["encodeIoctlIn", "fuseEncodeIoctlIn"],
+    ["decodeIoctlOut", "fuseDecodeIoctlOut"], ["encodeIoctlOut", "fuseEncodeIoctlOut"],
     ["decodePollIn", "fuseDecodePollIn"], ["encodePollIn", "fuseEncodePollIn"],
     ["decodePollOut", "fuseDecodePollOut"], ["encodePollOut", "fuseEncodePollOut"],
-    ["decodeSetxattrIn", "fuseDecodeSetxattrIn"], ["encodeSetxattrIn", "fuseEncodeSetxattrIn"],
-    ["decodeGetxattrIn", "fuseDecodeGetxattrIn"], ["encodeGetxattrIn", "fuseEncodeGetxattrIn"],
-    ["decodeListxattrIn", "fuseDecodeListxattrIn"], ["encodeListxattrIn", "fuseEncodeListxattrIn"],
-    ["decodeRemovexattrIn", "fuseDecodeRemovexattrIn"], ["encodeRemovexattrIn", "fuseEncodeRemovexattrIn"],
     ["decodeGetxattrOut", "fuseDecodeGetxattrOut"], ["encodeGetxattrOut", "fuseEncodeGetxattrOut"],
     ["encodeXattrNames", "fuseEncodeXattrNames"], ["decodeXattrNames", "fuseDecodeXattrNames"],
   ]) {
@@ -736,9 +734,6 @@ function install(binding) {
       }
       if (publicName === "encodeReadOut" && args[0] !== undefined) {
         args[0] = { ...args[0], data: copyBytes(args[0].data) }
-      }
-      if (publicName === "encodeSetxattrIn" && args[0] !== undefined) {
-        args[0] = { ...args[0], value: copyBytes(args[0].value) }
       }
       if (args[0] && args[0].buffer !== undefined && typeof args[0] !== "object") args[0] = copyBytes(args[0])
       try {
